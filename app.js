086bb26022ce6a1a05f8522f8953c8de130efeb9
fmmyriam <benhmyriam@gmail.com>
2021-03-08 15:09:13 +0100

    created routes in app.js and separate routes

--- app.js
+++ app.js
@@ -7,10 +7,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const authRouter = require('./routes/auth');
+const birthdaysRouter = require('./routes/birthdays');
+const giftsRouter = require('./routes/gifts');
 
 var app = express();
 
@@ -25,7 +28,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', usersRouter);
+app.use('/', authRouter);
+app.use('/', birthdaysRouter);
+app.use('/', giftsRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
