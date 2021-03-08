bb0aad46579555f3e8f733e766051cfb1e9358c8
Juan Falconi <juanfalconi86@gmail.com>
2021-03-08 17:38:23 +0100

    Route pour DISPLAY les Gifts et HBS concerné crée

--- routes/gifts.js
+++ routes/gifts.js
@@ -1,4 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const GiftModel = require('../models/gifts')
+
+
+//ROUTE TO DISPLAY THE LIST OF EXISTING GIFTS
+router.get('/gifts', (req, res, next)=>{
+    GiftModel.find()
+    .then((dbGifts)=>{
+        res.render('partials/gifts', {dbGifts})
+    })
+    .catch((error)=>{
+        log(error)
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
