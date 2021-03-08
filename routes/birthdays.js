7c636ca82fe90eb72064ea479786c25da43ad8f0
fmmyriam <benhmyriam@gmail.com>
2021-03-08 17:00:47 +0100

    started routes and birthday create form

--- routes/birthdays.js
+++ routes/birthdays.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+// CREATE BIRTHDAY
 router.get("/birthday/create", (req, res, next) => {
-    res.render("birthday-create.hbs");
+    res.render("birthday_create.hbs");
 });
 
 router.post("/birthday/create", (req, res, next) => {
     res.redirect("/birthdays");
+});
+
+// UPDATE BIRTHDAY
+router.get("/birthday/edit", (req, res, next) => {
+    res.render("birthday_edit.hbs");
 });
 
 module.exports = router;
