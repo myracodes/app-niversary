c374f057c5a510002ba4bea3a3faf1814eb0e9e6
fmmyriam <benhmyriam@gmail.com>
2021-03-08 16:35:19 +0100

    basic css done and created create form hbs

--- routes/birthdays.js
+++ routes/birthdays.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+router.get("/birthday/create", (req, res, next) => {
+    res.render("birthday-create.hbs");
+});
+
+router.post("/birthday/create", (req, res, next) => {
+    res.redirect("/birthdays");
+});
+
 module.exports = router;
