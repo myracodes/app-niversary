47ada936b6db446bfd63efa2b608ace9f3dd5058
fmmyriam <benhmyriam@gmail.com>
2021-03-08 21:14:37 +0100

    added navbar

--- routes/birthdays.js
+++ routes/birthdays.js
@@ -1,13 +1,48 @@
 const express = require('express');
 const router = express.Router();
+const BirthdayModel = require('./../models/birthdays');
+const GiftModel = require('./../models/gifts');
+
+// BIRTHDAYS DASHBOARD
+router.get('/dashboard', (req, res, next) => {
+    BirthdayModel.find()
+        .then((birthdays) => {
+            res.render('birthdays.hbs', {
+                birthdays
+            });
+        })
+        .catch((error) => {
+            console.log(error, 'error with tag enregistrement');
+        });
+});
 
 // CREATE BIRTHDAY
 router.get("/birthday/create", (req, res, next) => {
     res.render("birthday_create.hbs");
+    console.log('-------------post create GET');
 });
 
-router.post("/birthday/create", (req, res, next) => {
-    res.redirect("/birthdays");
+router.post("/birthday/create", async (req, res, next) => {
+    console.log('-------------post create');
+    const {
+        friendName,
+        birthday,
+        picture,
+        gift
+    } = req.body;
+    try {
+        await BirthdayModel.create({
+            friendName,
+            birthday,
+            picture,
+            gift
+        });
+        console.log("birthday successfully created :D");
+        res.redirect("/birthdays");
+    } catch (err) {
+        next(err);
+        res.render("error.hbs");
+    }
 });
 
 // UPDATE BIRTHDAY
