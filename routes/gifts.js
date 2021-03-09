ef3ac6168ac6c234ce3670fdf60fa0fd817a727f
Juan Falconi <juanfalconi86@gmail.com>
2021-03-09 13:03:12 +0100

    modifié le enctype dans le view et modifié la route create gifts

--- routes/gifts.js
+++ routes/gifts.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const GiftModel = require('../models/gifts')
+const fileUploader = require('../config/cloudinary')
+
 
 
 //ROUTE TO DISPLAY THE LIST OF EXISTING GIFTS
@@ -23,9 +25,14 @@ router.get('/gifts/create', (req, res, next)=>{
 })
 
 //here, to post the informaton of the gift
-router.post("/gifts/create", (req, res, next)=>{
-    GiftModel.create(req.body)
+router.post("/gifts/create", fileUploader.single("Picture"), (req, res, next)=>{
+    console.log("-----JE RENTRE DANS LE CREATE --------");
+    const newGift = { ...req.body };
+    if (!req.file) newGift.Picture = undefined;
+    else newGift.Picture = req.file.path;
+    GiftModel.create(newGift)
     .then(()=>{
+        console.log(req.body, "----- J AI BIEN CRÉE LE CADEAU-----");
         res.redirect('/gifts')
     })
     .catch((error)=>{
@@ -34,6 +41,7 @@ router.post("/gifts/create", (req, res, next)=>{
 })
 
 //ROUTE TO UPDATE GIFT
+//first, we get the form with the ID of the gift and its values
 router.get("/gifts/update/:id", (req, res, next)=>{
     GiftModel.findById(req.params.id)
     .then((dbGifts)=>{
@@ -43,6 +51,30 @@ router.get("/gifts/update/:id", (req, res, next)=>{
         console.log(error)
     })
 })
+//Now we'll post the information updated
+router.post("/gifts/update/:id", (req, res, next)=>{
+    console.log("-------RENTRE DANS UPDATE---------");
+    GiftModel.findByIdAndUpdate(req.params.id, req.body)
+    .then(()=>{
+        res.redirect("/gifts");
+    })
+    .catch((error)=>{
+        res.redirect("/gifts/update/:id", error);
+    })
+});
+
+//ROUTE TO DELETE THE GIFT
+router.post("/gifts/delete/:id", (req, res, next) => {
+  GiftModel.findByIdAndDelete(req.params.id)
+    .then(() => {
+        console.log("-----JE DELETE BIEN-----");
+      res.redirect("/gifts");
+    })
+    .catch((error) => {
+        console.log("------JE RENTRE PAS DANS LE DELETE-----");
+      console.log(error);
+    });
+});
 
 
 
